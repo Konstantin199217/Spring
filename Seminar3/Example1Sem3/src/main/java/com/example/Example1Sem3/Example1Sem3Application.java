bddedb2f65444e5212e94d3884f14df8098d2f12
Konstantin199217 <112700835+Konstantin199217@users.noreply.github.com>
2024-02-04 14:21:06 +0900

    Sem3

---
+++ Seminar3/Example1Sem3/src/main/java/com/example/Example1Sem3/Example1Sem3Application.java
@@ -0,0 +1,13 @@
+package com.example.Example1Sem3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example1Sem3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example1Sem3Application.class, args);
+	}
+wweq
+}
---
+++ Seminar3/Example1Sem3/src/test/java/com/example/Example1Sem3/Example1Sem3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Example1Sem3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example1Sem3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
