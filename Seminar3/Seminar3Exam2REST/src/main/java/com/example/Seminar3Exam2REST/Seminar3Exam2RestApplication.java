bb09524a848d482219b951c5b45e8c23b07ee69d
Konstantin199217 <112700835+Konstantin199217@users.noreply.github.com>
2024-02-04 14:16:02 +0900

    sem3q2q

---
+++ Seminar3/Seminar3Exam2REST/src/main/java/com/example/Seminar3Exam2REST/Seminar3Exam2RestApplication.java
@@ -0,0 +1,13 @@
+package com.example.Seminar3Exam2REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar3Exam2RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar3Exam2RestApplication.class, args);
+	}
+
+}
---
+++ Seminar3/Seminar3Exam2REST/src/test/java/com/example/Seminar3Exam2REST/Seminar3Exam2RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Seminar3Exam2REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar3Exam2RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
